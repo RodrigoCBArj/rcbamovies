d7054157d5839a007d62ba9dedc54f2122262d34
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-01-11 19:36:23 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,11 @@
+import '../../index.css';
+
+function Form() {
+    return(
+        <html>
+            <h1>Página de formulário</h1>
+        </html>
+    )
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,11 @@
+import '../../index.css';
+
+function Listing() {
+    return(
+        <html>
+            <h1>Página de listagem</h1>
+        </html>
+    )
+}
+
+export default Listing;

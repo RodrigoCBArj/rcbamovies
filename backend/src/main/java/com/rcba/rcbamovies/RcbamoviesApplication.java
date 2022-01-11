15531a01fbefa7e55b643063d8af84d486af84dc
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-01-11 10:53:27 -0300

    Project created

---
+++ backend/src/main/java/com/rcba/rcbamovies/RcbamoviesApplication.java
@@ -0,0 +1,13 @@
+package com.rcba.rcbamovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcbamoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcbamoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rcba/rcbamovies/RcbamoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcba.rcbamovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcbamoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

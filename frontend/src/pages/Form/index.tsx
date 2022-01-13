18df13c9da6147f502ad5d56664888b889b3e435
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-01-12 22:15:57 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 function Form() {
@@ -11,40 +12,40 @@ function Form() {
   };
 
   return (
-      <div className="rcbamovies-form-container">
-        <img
-          className="rcbamovies-movie-card-image"
-          src={movie.image}
-          alt={movie.title}
-        />
-        <div className="rcbamovies-card-bottom-container">
-          <h3>{movie.title}</h3>
-          <form className="rcbamovies-form">
-            <div className="form-group rcbamovies-form-group">
-              <label htmlFor="email">Informe seu email</label>
-              <input type="email" className="form-control" id="email" />
-            </div>
-            <div className="form-group rcbamovies-form-group">
-              <label htmlFor="score">Informe sua avaliação</label>
-              <select className="form-control" id="score">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-            </div>
-            <div className="rcbamovies-form-btn-container">
-              <button type="submit" className="rcbamovies-btn">
-                Salvar
-              </button>
-            </div>
-          </form>
-          <button className="rcbamovies-btn mt-3">
-            Cancelar
-          </button>
-        </div>
+    <div className="rcbamovies-form-container">
+      <img
+        className="rcbamovies-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <div className="rcbamovies-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <form className="rcbamovies-form">
+          <div className="form-group rcbamovies-form-group">
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <div className="form-group rcbamovies-form-group">
+            <label htmlFor="score">Informe sua avaliação</label>
+            <select className="form-control" id="score">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="rcbamovies-form-btn-container">
+            <button type="submit" className="rcbamovies-btn">
+              Salvar
+            </button>
+          </div>
+        </form>
+        <Link to="/">
+          <button className="rcbamovies-btn mt-3">Cancelar</button>
+        </Link>
       </div>
+    </div>
   );
 }
 
